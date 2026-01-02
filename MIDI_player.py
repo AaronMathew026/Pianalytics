56ce979e492903115e34a505447b5f0838ceede2
Aaron <aaron.mathew@student.manchester.ac.uk>
2026-01-02 18:53:56 +0000

    Finished MIDI player

--- MIDI_player.py
+++ MIDI_player.py
@@ -1,35 +1,33 @@
 import pygame
-
-def play_music(midi_filename):
-  '''Stream music_file in a blocking manner'''
-  clock = pygame.time.Clock()
-  pygame.mixer.music.load(midi_filename)
-  pygame.mixer.music.play()
-  while pygame.mixer.music.get_busy():
-    clock.tick(30) # check if playback has finished
-    
-midi_filename = f'C:\\Users\\aaron\\Documents\\Projects\\Pianalytics\\Naishi.mid'
-
-# mixer config
-freq = 44100  # audio CD quality
-bitsize = -16   # unsigned 16 bit
-channels = 2  # 1 is mono, 2 is stereo
-buffer = 1024   # number of samples
-pygame.mixer.init(freq, bitsize, channels, buffer)
-
-# optional volume 0 to 1.0
-pygame.mixer.music.set_volume(0.8)
-
-# listen for interruptions
-try:
-  # use the midi file you just saved
-  play_music(midi_filename)
-except KeyboardInterrupt:
-  # if user hits Ctrl/C then exit
-  # (works only in console mode)
-  pygame.mixer.music.fadeout(1000)
-  pygame.mixer.music.stop()
-  raise SystemExit
+from tkinter import messagebox
 
 
-play_music(midi_filename)
+
+
+class music_player():
+  def __init__(self):
+    self.freq = 44100  # audio CD quality
+    self.bitsize = -16   # unsigned 16 bit
+    self.channels = 2  # 1 is mono, 2 is stereo
+    self.buffer = 1024   # number of samples
+    pygame.mixer.init(self.freq, self.bitsize, self.channels, self.buffer)
+
+    # optional volume 0 to 1.0
+    pygame.mixer.music.set_volume(0.8)
+
+  def play_music(self,midi_filename):
+    if midi_filename:
+      '''Stream music_file in a blocking manner'''
+      pygame.mixer.music.load(midi_filename)
+      pygame.mixer.music.play()
+    else:
+      messagebox.showerror("Error!",message=" No file Found!")
+
+      return False
+
+  def play_pause(self):
+    if pygame.mixer.music.get_busy():
+      pygame.mixer.music.pause()
+    else:
+      pygame.mixer.music.unpause()
+
--- main_window.py
+++ main_window.py
@@ -1,12 +1,30 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 from game_modes import Game_Select
-#from MIDI_player import play_music
+from MIDI_player import music_player
 import time
 import random
 class MainWindow(tk.Tk):
     def __init__(self):
+
+
+        # self.style = ttk.Style()
+        # self.style.configure(
+        #     "Custom.TButton",
+        #     font = ("Helvetica",12,"bold"),
+        #     foreground = "blue",
+        #     padding = 10
+
+        # )
+        # self.style.map(
+        #     "Custom.TButton",
+        #     foreground = [("pressed","white"),("active","red")],
+        #     background = [("pressed","#333"),("active","#ddd")]
+
+        # )
+        self.button_font = ("Helvetica",16)
         self.game_select = Game_Select()
         super().__init__()
         self.title("Pianalytics")
@@ -26,9 +44,18 @@ class MainWindow(tk.Tk):
         self.image_label = tk.Label(self, image=self.menu_image)
         self.image_label.pack(pady=20)
     def start_window(self):
+
+
         self.start_window = tk.Toplevel(self)
         self.start_window.title("Start Menu")
         self.start_window.geometry("400x300")
+       
+        start_menu = tk.Menu(self)
+        options_menu = tk.Menu(start_menu,tearoff= 0 )
+        options_menu.add_command(label= "Exit",command=self.exit_window)
+        start_menu.add_cascade(label = "Options",menu = options_menu)
+        self.start_window.config(menu = start_menu)
+
 
         self.start_window_label = tk.Label(self.start_window, text="Start Menu", font=("Helvetica", 14))
         self.start_window_label.pack(pady=20)
@@ -61,6 +88,7 @@ class MainWindow(tk.Tk):
         self.exit_button.pack(pady=10)
 
     def midi_play_window(self):
+        music = music_player()
         self.start_window.destroy()
         self.midi_play_window = tk.Toplevel(self)
         self.midi_play_window.title("MIDI Playback")
@@ -70,9 +98,11 @@ class MainWindow(tk.Tk):
 
         file_menu = tk.Menu(menu_bar, tearoff=0)
         file_menu.add_command(label = "Open", command = self.file_select_window)
-        file_menu.add_separator()
-        file_menu.add_command(label = "Exit", command = self.exit_window)
         menu_bar.add_cascade(label="File", menu=file_menu)
+
+        options_menu  = tk.Menu(menu_bar,tearoff= 0 )
+        options_menu.add_command(label = "Exit", command = self.exit_window)
+        menu_bar.add_cascade(label = "Options", menu = options_menu)
         self.midi_play_window.config(menu=menu_bar)
 
         self.midi_play_window_label = tk.Label(self.midi_play_window, text="MIDI Playback Mode", font=("Helvetica", 14))
@@ -80,17 +110,27 @@ class MainWindow(tk.Tk):
 
         self.song_title = tk.Label(self.midi_play_window,text = "Please Select a song")
         self.song_title.pack(fill = "both")
+        try:
+        
+            self.midi_play_button = tk.Button(self.midi_play_window, text="Play MIDI",command =  lambda: music.play_music(self.file_name) , font=("Helvetica",14))
+            self.midi_play_button.pack(fill="both")
+            self.pause_unpause_button = tk.Button(self.midi_play_window,text = "Pause/Unpause",command = music.play_pause, font = self.button_font )
+            self.pause_unpause_button.pack(fill = "both")
+        except AttributeError:
+            messagebox.showerror("Error!",message=" No file Found!")
+        self.midi_play_window.lift()
+        self.midi_play_window.attributes("-topmost",True)
     def file_select_window(self):
         try:
-            file_name = filedialog.askopenfilename(
+            self.file_name = filedialog.askopenfilename(
                 title = "Select a MIDI file to Play",
                 filetypes=[("MIDI Files", "*.mid")]
 
             )
-            if file_name:
+            if self.file_name:
                 messagebox.showinfo("Information", "File Successfully uploaded")
-                print(file_name)
-                self.song_title.config(text = f"{file_name}")
+                print(self.file_name)
+                self.song_title.config(text = f"{self.file_name}")
             return True
         except tk.TclError:
             messagebox.showerror("Error!","This file is not supported")
